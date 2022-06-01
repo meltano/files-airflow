64184a68e8d31ac3181ebdb90f30623ba16390a9
Florian Hines <syn@ronin.io>
2022-06-01 16:21:07 -0500

    First pass

--- bundle/orchestrate/dags/meltano.py
+++ bundle/orchestrate/dags/meltano.py
@@ -44,6 +44,122 @@ if not Path(project_root).joinpath(meltano_bin).exists():
     meltano_bin = "meltano"
 
 
+def _meltano_v1_generator(schedules):
+    """Generate singular dag's for each legacy Meltano elt task.
+
+    Args:
+        schedules (list): List of Meltano schedules.
+    """
+    for schedule in schedules:
+        logger.info(f"Considering schedule '{schedule['name']}': {schedule}")
+
+        if not schedule["cron_interval"]:
+            logger.info(
+                f"No DAG created for schedule '{schedule['name']}' because its interval is set to `@once`."
+            )
+            continue
+
+        args = DEFAULT_ARGS.copy()
+        if schedule["start_date"]:
+            args["start_date"] = schedule["start_date"]
+
+        dag_id = f"meltano_{schedule['name']}"
+
+        tags = DEFAULT_TAGS.copy()
+        if schedule["extractor"]:
+            tags.append(schedule["extractor"])
+        if schedule["loader"]:
+            tags.append(schedule["loader"])
+        if schedule["transform"] == "run":
+            tags.append("transform")
+        elif schedule["transform"] == "only":
+            tags.append("transform-only")
+
+        # from https://airflow.apache.org/docs/stable/scheduler.html#backfill-and-catchup
+        #
+        # It is crucial to set `catchup` to False so that Airflow only create a single job
+        # at the tail end of date window we want to extract data.
+        #
+        # Because our extractors do not support date-window extraction, it serves no
+        # purpose to enqueue date-chunked jobs for complete extraction window.
+        dag = DAG(
+            dag_id,
+            tags=tags,
+            catchup=False,
+            default_args=args,
+            schedule_interval=schedule["interval"],
+            max_active_runs=1,
+        )
+
+        elt = BashOperator(
+            task_id="extract_load",
+            bash_command=f"cd {project_root}; {meltano_bin} schedule run {schedule['name']}",
+            dag=dag,
+        )
+
+        # register the dag
+        globals()[dag_id] = dag
+
+        logger.info(f"DAG created for schedule '{schedule['name']}'")
+
+
+def _meltano_v2_job_generator(schedules):
+    """Generate dag's for each task within a Meltano scheduled job.
+
+    Args:
+        schedules (list): List of Meltano scheduled jobs.
+    """
+    for schedule in schedules:
+        if not schedule.get("job"):
+            logger.info(
+                f"No DAG's created for schedule '{schedule['name']}'. It was passed to job generator but has no job."
+            )
+            continue
+        if not schedule["cron_interval"]:
+            logger.info(
+                f"No DAG created for schedule '{schedule['name']}' because its interval is set to `@once`."
+            )
+            continue
+
+        base_id = f"meltano_{schedule['name']}_{schedule['job']['name']}"
+        common_tags = DEFAULT_TAGS.copy()
+        common_tags.append(schedule["job"]["name"])
+        for idx, task in enumerate(schedule["job"]["tasks"]):
+            logger.info(f"Considering task '{task}' of schedule '{schedule['name']}': {schedule}")
+            args = DEFAULT_ARGS.copy()
+
+            dag_id = f"{base_id}_task{idx}"
+
+            task_tags = common_tags.copy()
+
+            # from https://airflow.apache.org/docs/stable/scheduler.html#backfill-and-catchup
+            #
+            # It is crucial to set `catchup` to False so that Airflow only create a single job
+            # at the tail end of date window we want to extract data.
+            #
+            # Because our extractors do not support date-window extraction, it serves no
+            # purpose to enqueue date-chunked jobs for complete extraction window.
+            dag = DAG(
+                dag_id,
+                description=f"Meltano run task[{idx}]: '{task}'",
+                tags=task_tags,
+                catchup=False,
+                default_args=args,
+                schedule_interval=schedule["interval"],
+                max_active_runs=1,
+            )
+
+            elt = BashOperator(
+                task_id=task["name"],
+                bash_command=f"cd {project_root}; {meltano_bin} run {task}",
+                dag=dag,
+            )
+
+            # register the dag
+            globals()[dag_id] = dag
+            logger.info(f"Task DAG created for schedule '{schedule['name']}',Task='{task}' ")
+
+
 result = subprocess.run(
     [meltano_bin, "schedule", "list", "--format=json"],
     cwd=project_root,
@@ -53,54 +169,8 @@ result = subprocess.run(
 )
 schedules = json.loads(result.stdout)
 
-for schedule in schedules:
-    logger.info(f"Considering schedule '{schedule['name']}': {schedule}")
-
-    if not schedule["cron_interval"]:
-        logger.info(
-            f"No DAG created for schedule '{schedule['name']}' because its interval is set to `@once`."
-        )
-        continue
-
-    args = DEFAULT_ARGS.copy()
-    if schedule["start_date"]:
-        args["start_date"] = schedule["start_date"]
-
-    dag_id = f"meltano_{schedule['name']}"
-
-    tags = DEFAULT_TAGS.copy()
-    if schedule["extractor"]:
-        tags.append(schedule["extractor"])
-    if schedule["loader"]:
-        tags.append(schedule["loader"])
-    if schedule["transform"] == "run":
-        tags.append("transform")
-    elif schedule["transform"] == "only":
-        tags.append("transform-only")
-
-    # from https://airflow.apache.org/docs/stable/scheduler.html#backfill-and-catchup
-    #
-    # It is crucial to set `catchup` to False so that Airflow only create a single job
-    # at the tail end of date window we want to extract data.
-    #
-    # Because our extractors do not support date-window extraction, it serves no
-    # purpose to enqueue date-chunked jobs for complete extraction window.
-    dag = DAG(
-        dag_id,
-        tags=tags,
-        catchup=False,
-        default_args=args,
-        schedule_interval=schedule["interval"],
-        max_active_runs=1,
-    )
-
-    elt = BashOperator(
-        task_id="extract_load",
-        bash_command=f"cd {project_root}; {meltano_bin} schedule run {schedule['name']}",
-        dag=dag,
-    )
-
-    # register the dag
-    globals()[dag_id] = dag
-
-    logger.info(f"DAG created for schedule '{schedule['name']}'")
+if not (schedules.get("job") and schedules.get("elt")):
+    _meltano_v1_generator(schedules)
+else:
+    _meltano_v1_generator(schedules.get("elt"))
+    _meltano_v2_job_generator(schedules.get("job"))
